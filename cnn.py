e07b24fbff0b355fcbad7be800bc08e305dec265
LuciferArc <52685792+LuciferArc@users.noreply.github.com>
2019-07-11 17:43:56 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -14,14 +14,14 @@ from keras.optimizers import SGD,Adam
 import keras
 
 dataset_path = './dataset'
-text_filenames = ['0_negative.txt', '1_happy.txt',
-                  '2_angry.txt', '3_sad.txt']
+text_filenames = ['0_AI.txt', '1_AI.txt',
+                  '2_AI.txt', '3_AI.txt']
 
 # 原始数据的csv文件
-output_text_filename = 'raw_weibo_text.csv'
+output_text_filename = 'raw_AI_text.csv'
 
 # 清洗好的文本数据文件/
-output_cln_text_filename = 'clean_weibo_text.csv'
+output_cln_text_filename = 'clean_AI_text.csv'
 
 # 处理和清洗文本数据的时间较长，通过设置is_first_run进行配置
 # 如果是第一次运行需要对原始文本数据进行处理和清洗，需要设为True
